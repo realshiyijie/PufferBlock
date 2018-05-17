04dbc5167d32edd609e02e57545cc778a6878823
shiyijie <shiyj93@gmail.com>
2018-05-17 10:47:59 +0800

    完成初始化操作函数

--- server/action/network.go
+++ server/action/network.go
@@ -19,3 +19,59 @@ func networkUp() {
 	out, _ := exec.Command("/bin/bash", "-c", cmd).Output()
 	fmt.Println(string(out))
 }
+
+//创建通道
+func createChannel() {
+	cmd := "sh server.sh createChannel"
+	outAsBytes, _ := exec.Command("/bin/sh", "-c", cmd).Output()
+	out := string(outAsBytes)
+	fmt.Println(string(out))
+}
+
+//更新锚节点
+func updateAnchorPeers() {
+	cmd := "sh server.sh updateAnchorPeers"
+	outAsBytes, _ := exec.Command("/bin/sh", "-c", cmd).Output()
+	out := string(outAsBytes)
+	fmt.Println(string(out))
+}
+
+//安装链码
+func installChaincode() {
+	cmd := "sh server.sh installChaincode"
+	outAsBytes, _ := exec.Command("/bin/sh", "-c", cmd).Output()
+	out := string(outAsBytes)
+	fmt.Println(string(out))
+}
+
+//实例化链码
+func instantiateChaincode() {
+	cmd := "sh server.sh instantiateChaincode"
+	outAsBytes, _ := exec.Command("/bin/sh", "-c", cmd).Output()
+	out := string(outAsBytes)
+	fmt.Println(string(out))
+}
+
+//测试初始化用户
+func chaincodeInitUser() {
+	cmd := "sh server.sh chaincodeInitUser"
+	outAsBytes, _ := exec.Command("/bin/sh", "-c", cmd).Output()
+	out := string(outAsBytes)
+	fmt.Println(string(out))
+}
+
+//测试查询
+func chaincodeQuery() {
+	cmd := "sh server.sh chaincodeQuery"
+	outAsBytes, _ := exec.Command("/bin/sh", "-c", cmd).Output()
+	out := string(outAsBytes)
+	fmt.Println(string(out))
+}
+
+//测试交易
+func chaincodeInvoke() {
+	cmd := "sh server.sh chaincodeInvoke"
+	outAsBytes, _ := exec.Command("/bin/sh", "-c", cmd).Output()
+	out := string(outAsBytes)
+	fmt.Println(string(out))
+}
