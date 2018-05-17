a83919f924426b711d91200586213ef1d2204a06
shiyijie <shiyj93@gmail.com>
2018-05-17 17:01:40 +0800

    master

--- server/action/ledger.go
+++ server/action/ledger.go
@@ -2,42 +2,47 @@
 package action
 
 import (
+	"fmt"
 	"os/exec"
 	"strconv"
 )
 
 var command = "/bin/bash"
-var subCommand = "sh"
+var subCommand = "bash"
+var scriptFile = "script.sh"
 
 //初始化账户
 func initCC(name string) (Response, error) {
-	cmd := subCommand + " " + "server.sh" + " " + "initCC" + " " + name
+	cmd := subCommand + " " + "initCC.sh" + " " + "initCC" + " " + name
 	outAsBytes, err := exec.Command(command, "-c", cmd).Output()
 	if err != nil {
 		return Response{false, err.Error(), ""}, nil
 	}
 	out := string(outAsBytes)
+	fmt.Println(out)
 	return Response{true, "", out}, nil
 }
 
 //进行交易
 func invokeCC(name string, opName string, opAmount int) (Response, error) {
-	cmd := subCommand + " " + "server.sh" + " " + "invokeCC" + " " + opName + " " + strconv.Itoa(opAmount)
+	cmd := subCommand + " " + "invokeCC.sh" + " " + "invokeCC" + " " + opName + " " + strconv.Itoa(opAmount)
 	outAsBytes, err := exec.Command(command, "-c", cmd).Output()
 	if err != nil {
 		return Response{false, err.Error(), ""}, nil
 	}
 	out := string(outAsBytes)
+	fmt.Println(out)
 	return Response{true, "", out}, nil
 }
 
 //查询账户信息
 func queryCC(function string, opName string) (Response, error) {
-	cmd := subCommand + " " + "server.sh" + " " + "queryCC" + " " + function + " " + opName
+	cmd := subCommand + " " + "queryCC.sh" + " " + "queryCC" + " " + function + " " + opName
 	outAsBytes, err := exec.Command(command, "-c", cmd).Output()
 	if err != nil {
 		return Response{false, err.Error(), ""}, nil
 	}
 	out := string(outAsBytes)
+	fmt.Println(out)
 	return Response{true, "", out}, nil
 }
