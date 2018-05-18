880b7a79a642be7a8fcdb86ce9919ec82e82137d
shiyijie <shiyj93@gmail.com>
2018-05-18 17:30:55 +0800

    0518

--- server/websockets/doselect.go
+++ server/websockets/doselect.go
@@ -5,27 +5,37 @@ import "myrepo/PufferBlock/server/action"
 
 //分类解析请求
 func (req *Request) doSelect() (action.Response, error) {
+
 	//缺少类型检查
 	if req.Type == "" {
 		return action.Response{IfSuccessful: false, ErrInfo: "no such type", Result: ""}, nil
 	}
+
 	//初始化账户
-	if req.Type == "initCC" {
-		return action.InitCC(req.Peer, req.Name)
+	if req.Type == "initUser" {
+		return action.InitUser(req.Peer, req.Name)
 	}
-	//查询账户信息
-	if req.Type == "queryCC" {
-		return action.QueryCC(req.Peer, req.Function, req.OpName)
-	}
+
 	//进行交易
-	if req.Type == "invokeCC" {
+	if req.Type == "invoke" {
 
 		//拒接非法交易
 		if req.Name != req.OpName {
 			return action.Response{IfSuccessful: false, ErrInfo: "denied", Result: ""}, nil
 		}
-		return action.InvokeCC(req.Peer, req.Name, req.OpName, req.OpAmount)
+		return action.Invoke(req.Peer, req.Name, req.OpName, req.OpAmount)
 	}
+
+	//查询账户信息
+	if req.Type == "queryUser" {
+		return action.QueryUser(req.Peer, req.OpName)
+	}
+
+	//
+	if req.Type == "queryAll" {
+		return action.QueryAll(req.Peer)
+	}
+
 	//类型错误抛出
 	return action.Response{IfSuccessful: false, ErrInfo: "wrong type", Result: ""}, nil
 }
