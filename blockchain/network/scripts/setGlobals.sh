79d834c88c98e9b7fb3096f3ef9f12808bbdb7ae
shiyijie <shiyj93@gmail.com>
2018-06-09 17:42:47 +0800

    0609

--- blockchain/network/scripts/setGlobals.sh
+++ blockchain/network/scripts/setGlobals.sh
@@ -12,10 +12,8 @@ setGlobals () {
 		CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 		if [ $PEER -eq 0 ]; then
 			CORE_PEER_ADDRESS=peer0.org1.example.com:7051
-			echo "PEER${PEER} is ready"
 		else
 			CORE_PEER_ADDRESS=peer1.org1.example.com:7051
-			echo "PEER${PEER} is ready"
 		fi
 	else
 		CORE_PEER_LOCALMSPID="Org2MSP"
@@ -23,10 +21,8 @@ setGlobals () {
 		CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
 		if [ $PEER -eq 2 ]; then
 			CORE_PEER_ADDRESS=peer0.org2.example.com:7051
-			echo "PEER${PEER} is ready"
 		elif [ $PEER -eq 3 ]; then
 			CORE_PEER_ADDRESS=peer1.org2.example.com:7051
-			echo "PEER${PEER} is ready"
 		else
 			arg1Help
 	fi
